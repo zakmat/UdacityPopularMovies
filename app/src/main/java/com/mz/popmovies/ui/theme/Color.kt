1d6166d5cee3a2ff69f0d5cd1a307932fb868a7f
Mateusz Żak <mateusz.zak@gmail.com>
2021-11-01 12:20:30 +0100

    prepare theme

---
+++ app/src/main/java/com/mz/popmovies/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.mz.popmovies.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val MediumGray = Color(0xFF505050)
+val ColorPrimary = Color(0xFF01D277)
+val TextWhite = Color(0xFFEEEEEE)
