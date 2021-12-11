a9fbaaa688cb5f70645ce26253c26d53a0f2ebf2
Mateusz Żak <mateusz.zak@gmail.com>
2021-12-11 20:31:45 +0100

    prepare migration to Gradle Kotlin DSL

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    plugins {
+        id ("com.android.application") version "7.2.0-alpha05" apply false
+        id ("com.android.library") version "7.2.0-alpha05" apply false
+        id ("org.jetbrains.kotlin.android") version "1.5.31" apply false
+        id ("org.jetbrains.kotlin.plugin.serialization") version "1.5.31" apply false
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+include (":app")
